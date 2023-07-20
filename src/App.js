040f42cdb922275982587454d244a55fd49a1980
SauravBeginner <kundusaurav1996@gmail.com>
2023-07-21 02:25:30 +0530

    web site commit

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="v904_492">
+      {/* <div className="v904_493">
+        <div className="v904_494">
+          <div className="v904_495">
+            <div className="v904_496" />
+          </div>
+          <div className="v904_497">
+            <div className="v904_498">
+              <div className="v904_499">
+                <div className="v904_500">
+                  <span className="v904_501">EN</span>
+                  <div className="v904_502">
+                    <div className="v904_503">
+                      <div className="v904_504">
+                        <div className="v904_505" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="v904_506">
+                <div className="v904_507">
+                  <div className="v904_508">
+                    <div className="v904_509">
+                      <div className="v904_510" />
+                      <div className="v904_511" />
+                      <div className="v904_512" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div> */}
+
+      <div className="v904_513">
+        <div className="v904_514">
+          <span className="v904_515">Play on Narratave.com</span>
+          <div className="v904_516">
+            <span className="v904_517">
+              Embrace the Joy of Stories: Listen, Read, and Explore Limitless
+              Books for Your Leisure Moments.
+            </span>
+          </div>
+          <span className="v904_518">Sign in to continue.</span>
+          <div className="v904_519">
+            <span className="v904_520">Install the application</span>
+          </div>
+        </div>
+      </div>
+      {/* <div className="v904_521">
+        <div className="v904_522" />
+        <div className="v904_523">
+          <span className="v904_524">Terms and Privacy Notice</span>
+          <span className="v904_525">Cookie Notice</span>
+          <span className="v904_526">Send us feedback</span>
+          <span className="v904_527">Help</span>
+        </div>
+        <span className="v904_528">
+          © 1996-2023, Amazon.com, Inc. or its affiliates
+        </span>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
